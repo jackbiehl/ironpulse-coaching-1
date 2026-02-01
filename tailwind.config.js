550d5f3cab3160aed26cbe973609b8082af2afb1
jackbiehl <jackrbiehl@gmail.com>
2026-02-01 06:02:17 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ironRed: "#dc2626",
+      },
+    },
+  },
+  plugins: [],
+};
